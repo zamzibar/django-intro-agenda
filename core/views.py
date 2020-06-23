c9eca2ca78549e4a6028ccc47d977532d627f0ca
Marcos Duchinski <mduchinski@gmail.com>
2020-06-23 19:10:24 -0300

    criada a tela de listagem de eventos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from core.models import Evento
 
 # Create your views here.
+def ver_evento(request,nome_evento):
+    consulta = Evento.objects.get(titulo=nome_evento)
+    return HttpResponse('O envento {} está marcado para {}'.format(consulta.titulo, consulta.data_evento))
+
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
+    dados = {'eventos':evento}
+    return render(request,'agenda.html', dados)
+
+# def index(request):
+#     return redirect('/agenda/')
