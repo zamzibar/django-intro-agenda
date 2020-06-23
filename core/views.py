8f74adf30d783261e9cbfd26285057584fef4590
Marcos Duchinski <mduchinski@gmail.com>
2020-06-23 20:53:10 -0300

    criada a tela de autenticação

--- core/views.py
+++ core/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def ver_evento(request,nome_evento):
     consulta = Evento.objects.get(titulo=nome_evento)
     return HttpResponse('O envento {} está marcado para {}'.format(consulta.titulo, consulta.data_evento))
 
+@login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario)
@@ -14,3 +18,27 @@ def lista_eventos(request):
 
 # def index(request):
 #     return redirect('/agenda/')
+
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+
+        if usuario is not None:
+            login(request, usuario)
+        else:
+            messages.error(request, "Usuário ou senha inválida")
+
+        return redirect('/')
+
+    else:
+        return redirect('/')
